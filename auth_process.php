d9a150f36ec2db2eb3f7efee2f9a1a130781e624
Matheus Pinheiro <matheusedulimapinheiro@gmail.com>
2024-04-23 16:32:32 -0300

    Startando autenticacao dos usuarios

--- auth_process.php
+++ auth_process.php
@@ -55,5 +55,25 @@ if ($type === "register") {
         // Enviar uma mensagem de erro , de dados faltantes
         $message->setMessage("Por favor , preencha todos os campos", "error", "back");
     }
-} else if ($type === "login") {
-}
+} else if($type === "login") {
+
+    $email = filter_input(INPUT_POST, "email");
+    $password = filter_input(INPUT_POST, "password");
+
+    // Tenta autenticar usuário
+    if($userDao->authenticateUser($email, $password)) {
+
+      $message->setMessage("Seja bem-vindo!", "success", "editprofile.php");
+
+    // Redireciona o usuário, caso não conseguir autenticar
+    } else {
+
+      $message->setMessage("Usuário e/ou senha incorretos.", "error", "back");
+
+    }
+
+  } else {
+
+    $message->setMessage("Informações inválidas!", "error", "index.php");
+
+  }
